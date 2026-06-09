70c5f3305b6a25dd078088fe11fea2951956d56a
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-09 22:36:09 +0000

    reset passage on level change

--- write.py
+++ write.py
@@ -1,8 +1,8 @@
 f=open('readlead.html','r')
 h=f.read()
 f.close()
-old='var ebtn=document.getElementById("ebtn-"+s.id);if(ebtn){ebtn.addEventListener("click",function(){editLevel(s.id);});}'
-new='var ebtn=document.getElementById("ebtn-"+s.id);if(ebtn){ebtn.addEventListener("click",function(){editLevel(s.id);});}var pbtn=document.getElementById("pbtn-"+s.id);if(pbtn&&s.sessions&&s.sessions.length>0){pbtn.addEventListener("click",function(){showProg(s.id);});}'
+old='students[idx].level=l;\nstudents[idx].grade=LM[l][0];\nsaveStudents();\ndocument.body.removeChild(overlay);\nrdDash();\nloadDashCurr();'
+new='students[idx].level=l;\nstudents[idx].grade=LM[l][0];\nsaveStudents();\ndocument.body.removeChild(overlay);\nif(cur&&cur.id==id){cur=students[idx];pidx=0;}\nrdDash();\nloadDashCurr();'
 print('found:',old in h)
 h=h.replace(old,new)
 open('readlead.html','w').write(h)
