4d344fb4aff2a302f32de177d8ab8ace3f105572
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-10 00:19:08 +0000

    remove debug logs fix progress

--- write.py
+++ write.py
@@ -1,9 +1,7 @@
 f=open('readlead.html','r')
 h=f.read()
 f.close()
-old='function showProg(id){\nvar stu=students.find(function(s){return s.id===id;});\n'
-new='function showProg(id){\nconsole.log("showProg called with id:",id);\nvar stu=students.find(function(s){return s.id===id;});\nconsole.log("student found:",stu?stu.name:"not found");\n'
-print('found:',old in h)
-h=h.replace(old,new)
+h=h.replace('console.log("showProg called with id:",id);\n','')
+h=h.replace('console.log("student found:",stu?stu.name:"not found");\n','')
 open('readlead.html','w').write(h)
 print('done')
