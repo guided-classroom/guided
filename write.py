dc63822c295f3c875f03c4222abd88df2bd12199
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-10 18:54:54 +0000

    fix nested backtick

--- write.py
+++ write.py
@@ -2,8 +2,7 @@ f=open('rotator.html','r')
 h=f.read()
 f.close()
 lines=h.split('\n')
-lines[365]=lines[365].split('<br><button')[0]+'</div>'
+lines[365]='<div style="font-size:11px;color:#8B949E">${g.students.join(", ")}${currentMode==="skill"?" · "+g.weakSkill:""}</div>'
 h='\n'.join(lines)
 open('rotator.html','w').write(h)
 print('done')
-print('Get Activity:',h.count('Get Activity'))
