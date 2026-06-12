4f70090247dadd3e5df630c3d9ee3e1decadf7ec
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-12 15:32:48 +0000

    fix setup screen logo

--- write.py
+++
@@ -1,9 +0,0 @@
-f=open('rotator.html','r')
-h=f.read()
-f.close()
-old='.logo{font-family:\'Playfair Display\',serif;font-size:20px;font-weight:900;}'
-new='.logo{font-family:\'Playfair Display\',serif;font-size:22px;font-weight:900;color:#fff;}'
-print('found:',old in h)
-h=h.replace(old,new)
-open('rotator.html','w').write(h)
-print('done')
