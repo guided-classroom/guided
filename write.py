6bbbbe8b87c9e377e2024437d134366029814c9c
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-14 23:46:26 +0000

    fix ready to generate text color

--- write.py
+++ write.py
@@ -1,8 +1,8 @@
 f=open('rotator.html','r')
 h=f.read()
 f.close()
-old='div class="logo" id="setup-logo">Centre<span>Rotator</span></div>'
-new='div class="logo" id="setup-logo" style="color:#1A1A2E">Centre<span style="color:#4ECDC4">Rotator</span></div>'
+old='<div style="font-size:17px;font-weight:800;margin-bottom:4px">Ready to generate?</div>'
+new='<div style="font-size:17px;font-weight:800;margin-bottom:4px;color:#fff">Ready to generate?</div>'
 print('found:',old in h)
 h=h.replace(old,new)
 open('rotator.html','w').write(h)
