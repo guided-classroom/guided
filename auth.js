167b57f6021efaf9510f3d849d647fbefe5c3261
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-15 16:38:16 +0000

    add login page

---
+++ auth.js
@@ -0,0 +1,5 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+export const supabase = createClient(
+"https://ynvgikozznybuuusxbhz.supabase.co",
+"sb_publishable_tt1xUdoOfZ0Pf8rXuMxyqw_hRfCzxeZ"
+)
