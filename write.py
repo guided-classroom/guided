f93a40503966d7764e559cc8e1d8426d18431223
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-15 14:12:21 +0000

    fix setup logo color

--- write.py
+++
@@ -1,9 +0,0 @@
-f=open('rotator.html','r')
-h=f.read()
-f.close()
-old='id="setup-logo" style="color:#1A1A2E">Centre<span style="color:#4ECDC4">Rotator</span>'
-new='id="setup-logo" style="color:#fff">Centre<span style="color:#A78BFA">Rotator</span>'
-print('found:',old in h)
-h=h.replace(old,new)
-open('rotator.html','w').write(h)
-print('done')
