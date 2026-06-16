b8bfee71a5674dd65b4b7b5fd8ede7e4d4240d81
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-16 15:06:20 +0000

    switch to UDM supabase

--- write.py
+++ write.py
@@ -1,9 +1,7 @@
 f=open('readlead.html','r')
 h=f.read()
 f.close()
-i=h.find('async function editLevel')
-j=h.find('\nasync function ',i+1)
-print('removing lines',h[:i].count('\n')+1,'to',h[:j].count('\n')+1)
-h=h[:i]+h[j:]
-open('readlead.html','w').write(h)
-print('done editLevel count:',h.count('function editLevel'))
+old='function initSupabase(){\nif(window._sbClient){\n_supabase=window._sbClient;\nwindow._supabase=_supabase;\nloadStudents().then(function(){loadDashCurrFromDB().then(function(){rdDash();loadDashCurr();});});\n}else{\nwindow.addEventListener("sb-ready",function(){\n_supabase=window._sbClient;\nwindow._supabase=_supabase;\nloadStudents().then(function(){loadDashCurrFromDB().then(function(){rdDash();loadDashCurr();});});\n});\n}\n}'
+new='function initSupabase(){\n_supabase=supabase.createClient("https://ynvgikozznybuuusxbhz.supabase.co","sb_publishable_tt1xUdoOfZ0Pf8rXuMxyqw_hRfCzxeZ");\nwindow._supabase=_supabase;\n_supabase.auth.getSession().then(function(result){\nvar session=result.data.session;\nif(!session){window.location.href="login.html";return;}\nwindow._user=session.user;\nloadStudents().then(function(){loadDashCurrFromDB().then(function(){rdDash();loadDashCurr();});});\n});\n}'
+print('found:',old in h)
+h=h.replace(old,new)
