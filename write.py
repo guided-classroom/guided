498ecf5c22c607a873d618e76de2cc5cc55a193a
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-16 12:14:34 +0000

    fix async functions

--- write.py
+++ write.py
@@ -1,9 +1,11 @@
 f=open('readlead.html','r')
 h=f.read()
 f.close()
-old='function initSupabase(){\nwindow.addEventListener("sb-ready",async function(){\n_supabase=window._sbClient;\nwindow._supabase=_supabase;\nawait loadStudents();\nawait loadDashCurrFromDB();\nrdDash();\nloadDashCurr();\n});\n}'
-new='function initSupabase(){\nif(window._sbClient){\n_supabase=window._sbClient;\nwindow._supabase=_supabase;\nloadStudents().then(function(){loadDashCurrFromDB().then(function(){rdDash();loadDashCurr();});});\n}else{\nwindow.addEventListener("sb-ready",function(){\n_supabase=window._sbClient;\nwindow._supabase=_supabase;\nloadStudents().then(function(){loadDashCurrFromDB().then(function(){rdDash();loadDashCurr();});});\n});\n}\n}'
-print('found:',old in h)
-h=h.replace(old,new)
+h=h.replace('function saveSessionOnly(){','async function saveSessionOnly(){')
+h=h.replace('function deleteStu(id){','async function deleteStu(id){')
+h=h.replace('function editLevel(id){','async function editLevel(id){')
 open('readlead.html','w').write(h)
 print('done')
+print('async saveSessionOnly:','async function saveSessionOnly' in h)
+print('async deleteStu:','async function deleteStu' in h)
+print('async editLevel:','async function editLevel' in h)
