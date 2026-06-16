72d3254557223d9bc4ee4b65ec1879535eb7aafb
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-16 00:00:09 +0000

    add sign out to all apps

--- write.py
+++ write.py
@@ -1,9 +1,9 @@
-f=open('readlead.html','r')
+f=open('progress.html','r')
 h=f.read()
 f.close()
-old='populateLvl();\nrdDash();'
-new='populateLvl();\nrdDash();\nwindow.doSignOut=async function(){\nvar sb=window._supabase;\nif(sb)await sb.auth.signOut();\nlocalStorage.clear();\nwindow.location.href="login.html";\n}'
+old='rdHome();\n</script>'
+new='rdHome();\nwindow.doSignOut=async function(){localStorage.clear();window.location.href="login.html";};\n</script>'
 print('found:',old in h)
 h=h.replace(old,new)
-open('readlead.html','w').write(h)
+open('progress.html','w').write(h)
 print('done')
