af0561186d63eb41a33631c4d14fc1a5a067e334
guided-classroom <Britjohnedwards35@gmail.com>
2026-06-17 23:02:08 +0000

    increase font size for students

--- write.py
+++ write.py
@@ -1,9 +1,9 @@
 f=open('readlead.html','r')
 h=f.read()
 f.close()
-old='PASSAGES["Level 1"]'
-new='var PASSAGES={};\nPASSAGES["Level 1"]'
+old="w.document.write(\"<html><body style='font-family:Georgia,serif;max-width:600px;margin:40px auto'><h1>\"+p.title+\"</h1><div style='white-space:pre-line;font-size:16px;line-height:2.2;margin-bottom:20px'>\"+p.text+\"</div>\""
+new="w.document.write(\"<html><body style='font-family:Georgia,serif;max-width:700px;margin:40px auto'><h1 style='font-size:32px'>\"+p.title+\"</h1><div style='white-space:pre-line;font-size:24px;line-height:2.4;margin-bottom:30px'>\"+p.text+\"</div>\""
 print('found:',old in h)
-h=h.replace(old,new,1)
+h=h.replace(old,new)
 open('readlead.html','w').write(h)
 print('done')
